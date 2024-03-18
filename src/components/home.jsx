f9de40f9eed5f3dfaa35267c0d40057b6c4486ba
Yahyobek <caisophiya@gmail.com>
2024-03-18 17:54:09 +0500

    Moby

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,9 +1,9 @@
 import '../styles/home.css'
 import logo from '../Images/Moby.png'
 import { Link } from 'react-router-dom'
-// import { FaLanguage } from "react-icons/fa6";
-// import { PiMicrophoneDuotone } from "react-icons/pi";
-// import { FcStackOfPhotos, FcNews   } from "react-icons/fc";
+import { FaLanguage } from "react-icons/fa6";
+import { PiMicrophoneDuotone } from "react-icons/pi";
+import { FcStackOfPhotos, FcNews   } from "react-icons/fc";
 
 
 const Home = () => {
@@ -21,19 +21,19 @@ const Home = () => {
         <div className="in-home-cards">
           <div className="home-card">
             <h3>Generate 3D Images With Moby AI Alse For Free</h3>
-            {/* <FcStackOfPhotos className='home-card-icon' /> */}
+            <FcStackOfPhotos className='home-card-icon' />
           </div>
           <div className="home-card">
             <h3>Give Answer Using Mickrophone For Moby AI</h3>
-            {/* <PiMicrophoneDuotone className='home-card-icon' /> */}
+            <PiMicrophoneDuotone className='home-card-icon' />
           </div>
           <div className="home-card">
             <h3>Translate World Sentences Using Moby AI</h3>
-            {/* <FaLanguage className='home-card-icon' /> */}
+            <FaLanguage className='home-card-icon' />
           </div>
           <div className="home-card">
             <h3>Know The Wolrd News In Moby AI</h3>
-            {/* <FcNews className='home-card-icon' /> */}
+            <FcNews className='home-card-icon' />
           </div>
         </div>
       </div>
