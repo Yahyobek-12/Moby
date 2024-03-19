80d7e5a3012239b049636d324d7b9e171bb64e61
Yahyobek <caisophiya@gmail.com>
2024-03-19 06:39:28 +0500

    Moby-AI

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,10 +2,10 @@ import 'animate.css';
 import '../styles/home.css'
 import logo from '../Images/Moby.png'
 import { Link } from 'react-router-dom'
+import Button from '@mui/material/Button'
 import { FaLanguage } from "react-icons/fa6";
 import { PiMicrophoneDuotone } from "react-icons/pi";
 import { FcStackOfPhotos, FcNews   } from "react-icons/fc";
-
 
 const Home = () => {
   return (
@@ -22,18 +22,22 @@ const Home = () => {
         <div className="in-home-cards">
           <div className="home-card animate__animated animate__fadeInUp">
             <h3>Generate 3D Images With Moby AI Alse For Free</h3>
+            <Link to="/"><Button variant='contained'>Get Started</Button></Link>
             <FcStackOfPhotos className='home-card-icon' />
           </div>
           <div className="home-card animate__animated animate__fadeInUp">
             <h3>Give Answer Using Mickrophone For Moby AI</h3>
+            <Link to="/"><Button variant='contained'>Get Started</Button></Link>
             <PiMicrophoneDuotone className='home-card-icon' />
           </div>
           <div className="home-card animate__animated animate__fadeInUp">
             <h3>Translate World Sentences Using Moby AI</h3>
+            <Link to="/"><Button variant='contained'>Get Started</Button></Link>
             <FaLanguage className='home-card-icon' />
           </div>
           <div className="home-card animate__animated animate__fadeInUp">
             <h3>Know The Wolrd News In Moby AI</h3>
+            <Link to="/"><Button variant='contained'>Get Started</Button></Link>
             <FcNews className='home-card-icon' />
           </div>
         </div>
