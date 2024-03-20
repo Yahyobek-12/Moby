50f247632f72349182bc3fcc7f05b72d49c86c81
Yahyobek <caisophiya@gmail.com>
2024-03-20 06:54:41 +0500

    Moby

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,6 +3,7 @@ import '../styles/home.css'
 import logo from '../Images/Moby.png'
 import { Link } from 'react-router-dom'
 import Button from '@mui/material/Button'
+import thunder from '../Images/Thunder.gif'
 import { FaLanguage } from "react-icons/fa6";
 import { PiMicrophoneDuotone } from "react-icons/pi";
 import { FcStackOfPhotos, FcNews   } from "react-icons/fc";
@@ -12,9 +13,9 @@ const Home = () => {
     <div className='home'>
       <div className="home-top">
         <div className="in-top">
-          <img src={logo} alt="" />
-          <h1>Hello Yahyobek</h1>
-          <h2>How Can I Help You Today ?</h2>
+          <img className='animate__animated animate__jackInTheBox' src={logo} alt="" />
+          <h1 className='animate__animated animate__fadeInDown'>Hello Yahyobek</h1>
+          <h2 className='animate__animated animate__fadeInUp'>How Can I Help You Today ?</h2>
         </div>
       </div>
 
@@ -24,7 +25,7 @@ const Home = () => {
             <h3>Generate 3D Images With Moby AI Alse For Free</h3>
             <Link to="/"><Button variant='contained'>Get Started</Button></Link>
             <FcStackOfPhotos className='home-card-icon' />
-          </div>
+          </div>  
           <div className="home-card animate__animated animate__fadeInUp">
             <h3>Give Answer Using Mickrophone For Moby AI</h3>
             <Link to="/"><Button variant='contained'>Get Started</Button></Link>
@@ -41,6 +42,13 @@ const Home = () => {
             <FcNews className='home-card-icon' />
           </div>
         </div>
+      </div>
+
+      <div className="footer">
+        <footer>
+          <Link to="https://yahyobek-12.vercel.app/" target="_blank">From Yahyobek-12</Link>
+          <img src={thunder} alt="" />
+        </footer>
       </div>
     </div>
   )
